c5d569e1d3504b0dd120850d20a6c453d34219c0
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-02-03 20:19:22 +0600

    add some js code for list item

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 // select the update button and working process code
 document.getElementById('btn-update').addEventListener('click', function(){
-    console.log('clicked');
+    const inputFieldValue = document.getElementById('task-filed');
+    const CreateLi = document.createElement('li');
+    CreateLi.innerText = inputFieldValue.value;
+    // console.log(CreateLi);
+    const listContainer = document.getElementById('List-container');
+    listContainer.appendChild(CreateLi);
+    inputFieldValue.value = ' ';
 });
